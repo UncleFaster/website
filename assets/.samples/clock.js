c836bcdf506e80d5b2776fb2b1be0ffedc51553b
UncleFaster <oleh.kovalenko.personal@gmail.com>
2024-10-23 14:12:34 +0300

    Update clock.js
    Add dete to updateClock

--- assets/.samples/clock.js
+++ assets/.samples/clock.js
@@ -1,8 +1,18 @@
+// (() => {
+//     function updateClock() {
+//         const clockContainer = document.querySelector('.clock');
+//         clockContainer.innerText = new Date().toLocaleTimeString('uk');
+//     }
+//     setInterval(updateClock, 1000);
+// })();
+
 (() => {
     function updateClock() {
         const clockContainer = document.querySelector('.clock');
-        clockContainer.innerText = new Date().toLocaleTimeString('uk');
+        const now = new Date();
+        const timeString = now.toLocaleTimeString('uk');
+        const dateString = now.toLocaleDateString('uk');
+        clockContainer.innerText = `${dateString} ${timeString}`;
     }
     setInterval(updateClock, 1000);
 })();
-
