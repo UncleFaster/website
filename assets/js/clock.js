c5d2957fcac617096a3740b534b3b6bf1fdccdfe
UncleFaster <oleh.kovalenko.personal@gmail.com>
2024-10-23 17:04:47 +0300

    Update clock.js
    delete old code

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -1,10 +1,3 @@
-// (() => {
-//     function updateClock() {
-//         const clockContainer = document.querySelector('.clock');
-//         clockContainer.innerText = new Date().toLocaleTimeString('uk');
-//     }
-//     setInterval(updateClock, 1000);
-// })();
 (() => {
     function updateClock() {
         const clockContainer = document.querySelector('.clock');
